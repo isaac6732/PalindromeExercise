a5b357c5999f1f4b5e7f19715b8f418208fcbe2d
Isaac Oquendo <ioalvarez378@gmail.com>
2024-02-08 20:15:29 -0500

    Palidrome

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+        public bool IsAPalindrome(string originalWord)
+        {
+            string reverseWord = "";
+            
+            for (int i = originalWord.Length - 1; i >= 0; i--)
+            {
+                reverseWord += originalWord[i];
+            }
+            return originalWord.ToLower() == reverseWord.ToLower();
+        }
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,3 +1,4 @@
+using PalindromeExercise;
 using System;
 using Xunit;
 
@@ -5,10 +6,18 @@ namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("racecar", true )]
+        [InlineData("hello", false  )]
+        [InlineData("level", true )]
+        [InlineData("radar", true )]
+        public void Test1(string word,bool expected)
         {
+            WordSmith intance = new WordSmith();
 
+            bool actual = intance.IsAPalindrome(word);
+
+            Assert.Equal(expected, actual);
         }
     }
 }
